fb7017e682aefe3ae5ff22623cf701c614709ebf
Goltsov94 <goltsov2pavel@gmail.com>
2023-08-16 17:08:37 +0300

    test 3

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log('Мы молодцы')
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return   Math.abs(dadYearsOld - sonYearsOld * 2)
+  }
+  console.log(twiceAsOld(50,10))
+  console.log(twiceAsOld(22,10))
+  console.log(twiceAsOld(22,15))
